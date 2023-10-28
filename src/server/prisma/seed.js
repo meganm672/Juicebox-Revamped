b171bf196b802933e01a8f3aed4aac6efe2b2d09
meganm672 <meganm672@gmail.com>
2023-10-28 12:30:38 -0400

    properly seeded the database tohave original users with the correct posts and tags

--- src/server/prisma/seed.js
+++ src/server/prisma/seed.js
@@ -4,58 +4,74 @@ const prisma = require("../../../client");
 async function main() {
   console.log("seeding the database");
   try {
-    const createUser = await prisma.users.createMany({
-      data: 
-        {
-          username: 'albert',
-          password: 'bertie99',
-          name: 'Al Bert',
-          location: 'Sidney, Australia',
-          posts:{
-            create:{
-              title: "First Post About Dogs",
-              content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
-              tags: ["#happy", "#youcandoanything"],
-              active: true,
-         
+    await prisma.users.create({
+      data: {
+        username: 'lucylulu',
+        password: 'kitten12345',
+        name: 'lucy lu',
+        location: 'Sidney, Australia',
+        active: true,
+        posts: {
+          create: {
+            title: "First Post",
+            content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
+            tags:{
+              create:[
+                {name:"#happy"}, 
+                {name:"#youcandoanything"}
+              ]
             }
-          },
-
-          active: true
+          }
         }
-    
-      //   {
-      //     username: 'sandra',
-      //     password: '2sandy4me',
-      //     name: 'Just Sandra',
-      //     location: 'Ain\'t tellin\'',
-      //     posts:{
-      //       create:{
-      //         title: "First Post About Cats",
-      //         content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
-      //         tags: ["#happy", "#youcandoanything", "#canmandoeverything"]
-      //       }
-      //     }
-      //   },
-      //   {
-      //     username: 'glamgal',
-      //     password: 'soglam',
-      //     name: 'Joshua',
-      //     location: 'Upper East Side',
-      //     posts:{
-      //       create:{
-      //         title: "First Post About Frogs",
-      //         content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
-      //         tags: ["#happy", "#worst-day-ever"]
-      //       }
-      //     }
-      //   }
-      // ]
+      },
     })
-    return createUser
-  } catch (error) {
-    console.error(error)
-  }
+    await prisma.users.create({
+      data: {
+        username: 'ariel',
+        password: 'mermaid25',
+        name: 'ariel',
+        location: 'Ain\'t tellin\'',
+        active: true,
+        posts: {
+          create: {
+            title: "How does this work?",
+            content: "Seriously, does this even do anything?",
+            tags:{
+              create:[
+                {name:"#happy"}, 
+                {name:"#worst-day-ever"}
+              ]
+            }
+          }
+        }
+      }
+    }),
+    await prisma.users.create({
+      data:{
+      username: 'fashioncat',
+      password: 'kitten123',
+      name: 'alvin',
+      location: 'Upper East Side',
+      active: true,
+      posts: {
+        create: {
+          title: "Living the Glam Life",
+        content: "Do you even? I swear that half of you are posing.",
+        tags:{
+          create:[
+            {name:"#happy"}, 
+            {name:"#youcandoanything"},
+            {name:"#canmandoeverything"}
+          ]
+        }
+        }
+      }
+    }
+    })
+      
+} catch (error) {
+  console.error(error)
+}
 }
 
 main()
